5524dfe1b6578f5248a09b2874c9f183681677d9
tito.delerino <f.titodelerino@gmail.com>
2022-05-17 16:15:17 -0300

    Modificar senha

---
+++ public_html/new_user.php
@@ -0,0 +1,13 @@
+<?php
+
+use \App\Entity\Cadastro;
+use \App\Controller\CadastroController;
+use App\Session\Session;
+
+require __DIR__ . '/vendor/autoload.php';
+
+
+
+include __DIR__ . '/App/Includes/page-header.php';
+include __DIR__ . '/App/Includes/page-cad-user.php';
+include __DIR__ . '/App/Includes/page-footer.php';
