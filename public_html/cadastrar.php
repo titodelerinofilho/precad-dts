2457ccc8788867763841e1c4d38d24107180e232
tito.delerino <f.titodelerino@gmail.com>
2022-06-06 12:42:25 -0300

    Adjust signup content

--- public_html/cadastrar.php
+++ public_html/cadastrar.php
@@ -10,8 +10,18 @@
 </head>
 <body>
     <div class="container-auto" id="main-container">
+        <div class="row align-items-center p-2" id="content-col">
+            <div class="col-md-auto text-center">
+                <img src="img/logo.png"/>
+            </div>
+            <div class="col text-center">
+                <h3>Cadastro para Clientes - DTS Distribuidora</h3>
+                <h5>Formulário de cadastro para clientes na DTS Distribuidora.</h5>
+                <h6>Verifique os campos atentamente e qualquer dúvida, mantenha contato conosco.</h6>
+            </div>
+        </div>
         <div class="row">
-            <div class="col-md-7 p-4" id="form-col">
+            <div class="col p-4" id="form-col">
                 <form method="post" action="src/SendCadastro.php" id="form-cad" onsubmit="validarForm()" enctype="multipart/form-data">
                     <div class="mb-3">
                         <div class="alert alert-danger" role="alert" style="display: none;" id="msg-erro">
@@ -138,20 +148,6 @@
                         </div>
                     </div>
                 </form>
-            </div>
-            <div class="col-md-5 p-4" id="content-col">
-                <div class="row" id="content-row">
-                    <div class="col-12 coluna text-center">
-                        <h3>Atualização de Cadastro</h3>
-                        <h5>Formulário para atualização de cadastro de clientes da DTS Distribuidora.</h5>
-                        <img src="img/cadastro.svg" alt="cadastro">
-                        <img src="img/LOGO_DTS.png" alt="dts">
-                    </div>
-                    <div class="col coluna text-center">
-                        <h3>Termo de Privacidade</h3>
-                        <p class="content lh-base text-start">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-                    </div>
-                </div>
             </div>
         </div>
     </div>
